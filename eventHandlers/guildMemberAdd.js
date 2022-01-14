ee6e0e8f3c051c296da5382bf8c9ef10092ce304
Sluggggg <97277864+Sluggggg@users.noreply.github.com>
2022-01-13 22:03:45 -0800

    Created guildMemberAdd.js
    Creates the guildMemberAdd event that will trigger when a user joins the server.

---
+++ eventHandlers/guildMemberAdd.js
@@ -0,0 +1,22 @@
+exports.handle = async function(guildMember) {
+    if (msg.author.bot) return;
+    
+    member.guild.channels.get('819963117148700692').send({ embed : {
+        color: ('#1433A6'),
+        title: "Welcome!",
+        description: "Welcome to deez " + member.user.username + " nutz gottem",
+        fields: [{ 
+            name: "Glizzard Entertainment",
+            value: "We are gamers/coders/doctors/artists, and so much more! We hope you stay with us and have a wonderful time and become part of our little family! :)"
+        }],
+        timestamp: newDate(),
+        footer: {
+            icon_url: client.user.avatarURL,
+            text: "© Glizzard Entertainment 2020 - The Future"
+        }
+    }});    
+    
+    const DMChannel = await guildMember.createDM();
+    
+    await DMChannel.send("This bot was created by Penicillen, and is now being co-dev'd by LucioBoi. Please read the rules, set up a profile using $profile, and don't be afraid to ask us questions! :)");
+}
