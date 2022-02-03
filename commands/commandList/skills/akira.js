7839dfa29561304b441f0a42b17b155d8acc2329
Adrian <adrian.lu95@gmail.com>
2022-02-02 18:28:41 -0800

    added akira command, still need to add images to pics folder

---
+++ commands/commandList/skills/akira.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: 'akira',
+  aliases: ['🐕'],
+	description: 'Dog.',
+
+	async execute(p) {
+		const akiraDir = './pics/akira';
+		const fs = require('fs');
+
+		fs.readdir(akiraDir, async(err, files) => {
+			const picID = randomizer(files.length);
+			const chosenPic = files[picID];
+			const pic = fs.readFileSync(akiraDir + chosenPic);
+
+			await p.send({files:[pic]});
+			await p.gobbler.users.addGlizzys(p.author.id, -5);
+		});
+	},
+};
+
+function randomizer(max) {
+  return Math.floor(Math.random() * (max - 1));
+}
