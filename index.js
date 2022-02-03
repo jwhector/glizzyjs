d93f7af7f05c4a1a4f3dfdc17a5d262e0c2845b4
jwhector <jared.hector@gmail.com>
2022-02-02 20:21:38 -0800

    Add Express app to listen to Heroku port

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const Gobbler = require('./Gobbler.js');
 // const { token } = require('./config.json');
 require('dotenv').config();
 
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 const bot = new Gobbler();
 
 // console.log(process.env.TOKEN);
