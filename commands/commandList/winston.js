47c00e922dac28dfcbe88e9d7a70a789b823c3e8
Adrian <adrian.lu95@gmail.com>
2022-02-09 20:11:10 -0800

    new winston command, needs images

---
+++ commands/commandList/winston.js
@@ -0,0 +1,28 @@
+const axios = require('axios').default;
+
+module.exports = {
+	name: 'winston',
+	description: 'White Fluff',
+
+	async execute(p) {
+		const path = 'pics/pets/winston';
+		const branch = '?ref=' + process.env.BRANCH;
+
+		const response = await axios.get(`https://api.github.com/repos/jwhector/glizzyjs/contents/${path}${branch}`);
+
+		const files = response.data;
+		const fileID = randomizer(files.length);
+		const chosenFile = files[fileID];
+		const pic = chosenFile.download_url;
+
+		await p.send(pic);
+		// const db_user = await p.getAuthor();
+		await p.gobbler.users.addGlizzys(p.author.id, -5);
+		// await p.gobbler.users.addGlizzys()
+		// IDEA: pay the animal owner when someone else fetches a picture of their pet
+	},
+};
+
+function randomizer(max) {
+  return Math.floor(Math.random() * (max - 1));
+}
