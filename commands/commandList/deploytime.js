785a2c802d7c1632bbaa8af5d28412de9f0770cd
jwhector <jared.hector@gmail.com>
2022-02-17 12:44:55 -0800

    Create deploytime.js command

---
+++ commands/commandList/deploytime.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'deploytime',
+	description: 'Show timestamp of most recent deploy.',
+	aliases: ['dt', 'ver'],
+	async execute(p) {
+		const fs = require('fs');
+		fs.readFile();
+	},
+};
