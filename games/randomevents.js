5ff264814a106d76e35e1d0a5480fc5a18618e2b
jwhector <jared.hector@gmail.com>
2022-03-09 00:03:43 -0800

    Fix ESLint errors

--- games/randomevents.js
+++ games/randomevents.js
@@ -1,4 +1,6 @@
-var Promise = require('bluebird');
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+const Promise = require('bluebird');
 
 async function randomEvent(message) {
 	const eventChannel = await message.guild.channels.create('Random event!', {
@@ -16,7 +18,7 @@ async function randomEvent(message) {
 	} catch (err) {
 		console.error(err);
 	}
-    let numPlayers = 0;
+	let numPlayers = 0;
 	const filter = (reaction) => reaction.emoji.name === '☄️';
 	const collector = msg.createReactionCollector(filter, { max: 1 });
 	collector.on('collect', async (reaction, reaction_user) => {
@@ -24,59 +26,55 @@ async function randomEvent(message) {
 		eventChannel.updateOverwrite(reaction_user.id, {
 			VIEW_CHANNEL: true
 		});
-        numPlayers++;
+		numPlayers++;
 	});
 
-    collector.on('end', async () => {
-        const event = new EmojiMatch(eventChannel);
-        await event.play();
-    });
+	collector.on('end', async () => {
+		const event = new EmojiMatch(eventChannel);
+		await event.play();
+	});
 
-    // RPS - 2 max
-    // Fastest reaction - no max, starts based on time
-    // First to match emoji - no max, starts based on time
+	// RPS - 2 max
+	// Fastest reaction - no max, starts based on time
+	// First to match emoji - no max, starts based on time
 
 }
 
 class EmojiMatch {
-    constructor(eventChannel) {
-        this.eventChannel = eventChannel;
-        this.emojis = ["✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"]
-    }
+	constructor(eventChannel) {
+		this.eventChannel = eventChannel;
+		this.emojis = ['✌','😂','😝','😁','😱','👉','🙌','🍻','🔥','🌈','☀','🎈','🌹','💄','🎀','⚽','🎾','🏁','😡','👿','🐻','🐶','🐬','🐟','🍀','👀','🚗','🍎','💝','💙','👌','❤','😍','😉','😓','😳','💪','💩','🍸','🔑','💖','🌟','🎉','🌺','🎶','👠','🏈','⚾','🏆','👽','💀','🐵','🐮','🐩','🐎','💣','👃','👂','🍓','💘','💜','👊','💋','😘','😜','😵','🙏','👋','🚽','💃','💎','🚀','🌙','🎁','⛄','🌊','⛵','🏀','🎱','💰','👶','👸','🐰','🐷','🐍','🐫','🔫','👄','🚲','🍉','💛','💚'];
+	}
 
-    getRandomEmoji() {
-    }
+	async play() {
+		const readyMsg = await this.eventChannel.send('Welcome to Emoji Match! The first to react with the matching emoji on the next message wins the event! React with a 🔑 when you\'re ready to go!');
 
-    async play() {
-        const readyMsg = await this.eventChannel.send('Welcome to Emoji Match! The first to react with the matching emoji on the next message wins the event! React with a 🔑 when you\'re ready to go!');
-
-        let readyCount = 0;
-        const filter = (reaction) => reaction.emoji.name === '🔑';
-        const collector = readyMsg.createReactionCollector(filter, { time: 120000, max: 1 });
-        collector.on('collect', async (reaction, reaction_user) => {
-            await this.eventChannel.send('yoloswag');
-        });
-        collector.on('end', async (collected) => {
-            const countdownMsg = await this.eventChannel.send('The event will begin in: 10');
-            let timeLeft = 9;
-            const countdown = async () => {
-                if (!timeLeft) {
-                    const randomEmoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
-                    const emojiMsg = await this.eventChannel.send(randomEmoji);
-                    const filter = (reaction) => reaction.emoji.name === randomEmoji;
-                    const emojiCollector = emojiMsg.createReactionCollector({ filter, time: 120000, max: 1 });
-                    emojiCollector.on('collect', async (reaction, reaction_user) => {
-                        await this.eventChannel.send(reaction_user.toString() + ' won the event! `50 xp` has been added to their account!');
-                    });
-                } else return Promise.delay(1000).then(async () => {
-                    await countdownMsg.edit('The event will begin in: ' + timeLeft);
-                    timeLeft--;
-                    countdown();
-                });
-            }
-            countdown();
-        })
-    }
+		const filter = (reaction) => reaction.emoji.name === '🔑';
+		const collector = readyMsg.createReactionCollector(filter, { time: 120000, max: 1 });
+		// collector.on('collect', async (reaction, reaction_user) => {
+		//     await this.eventChannel.send('yoloswag');
+		// });
+		collector.on('end', async (collected) => {
+			const countdownMsg = await this.eventChannel.send('The event will begin in: 10');
+			let timeLeft = 9;
+			const countdown = async () => {
+				if (!timeLeft) {
+					const randomEmoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
+					const emojiMsg = await this.eventChannel.send(randomEmoji);
+					const filter = (reaction) => reaction.emoji.name === randomEmoji;
+					const emojiCollector = emojiMsg.createReactionCollector(filter, { time: 120000, max: 1 });
+					emojiCollector.on('collect', async (reaction, reaction_user) => {
+						await this.eventChannel.send(reaction_user.toString() + ' won the event! `50 xp` has been added to their account!');
+					});
+				} else return Promise.delay(1000).then(async () => {
+					await countdownMsg.edit('The event will begin in: ' + timeLeft);
+					timeLeft--;
+					countdown();
+				});
+			};
+			countdown();
+		});
+	}
 }
 
-module.exports = { randomEvent }
+module.exports = { randomEvent };
